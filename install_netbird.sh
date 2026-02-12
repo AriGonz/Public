82d3e64af913785813cb3a478e6d6201f36548ba
AriGonz <95000925+AriGonz@users.noreply.github.com>
2026-02-12 16:46:02 -0600

    Update install_netbird.sh

--- install_netbird.sh
+++ install_netbird.sh
@@ -16,6 +16,9 @@ curl -sSL https://pkgs.netbird.io/debian/public.key | gpg --dearmor --output /us
 # Add NetBird repository
 echo 'deb [signed-by=/usr/share/keyrings/netbird-archive-keyring.gpg] https://pkgs.netbird.io/debian stable main' | tee /etc/apt/sources.list.d/netbird.list
 
+# Update package list
+apt-get update
+
 # Install NetBird
 apt-get install netbird
 
